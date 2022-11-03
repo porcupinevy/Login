56b4075f1580844051bd7db21831bd8c3298c69a
Pham Van Truong <phamtruong025879@gmail.com>
2022-11-03 20:12:37 +0700

    main

---
+++ src/main/java/com/vnfite/loginmicroapp/login_micro_app/PrjappApplication.java
@@ -0,0 +1,13 @@
+package com.vnfite.loginmicroapp.login_micro_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vnfite/loginmicroapp/login_micro_app/PrjappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnfite.loginmicroapp.login_micro_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
